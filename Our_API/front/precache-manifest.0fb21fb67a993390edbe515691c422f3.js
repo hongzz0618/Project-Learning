11fd63859e7ca097081b245c41cd77906c3299c6
Hong_zhou <zhougg98@gmail.com>
2019-09-26 13:52:00 +0200

    para heroku

---
+++ Our_API/front/precache-manifest.0fb21fb67a993390edbe515691c422f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c4310e0d9098ea9045bdb696a793cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f02c27ccca15a1f190d9",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "02e39c5a46032962b329",
+    "url": "/static/css/main.78e72a63.chunk.css"
+  },
+  {
+    "revision": "f02c27ccca15a1f190d9",
+    "url": "/static/js/2.20512901.chunk.js"
+  },
+  {
+    "revision": "02e39c5a46032962b329",
+    "url": "/static/js/main.48b3eeb9.chunk.js"
+  },
+  {
+    "revision": "54a1ca36276677b62ee8",
+    "url": "/static/js/runtime~main.419c25e5.js"
+  }
+]);
